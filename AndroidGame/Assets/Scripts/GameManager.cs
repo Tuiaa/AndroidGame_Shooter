8644ad4a72f368941b12ec0b958548a30639a9e6
Minja Silvennoinen <minjasilvennoinen@gmail.com>
2016-08-04 15:06:48 +0300

    Added GameManager

--- AndroidGame/Assets/Scripts/GameManager.cs
+++ AndroidGame/Assets/Scripts/GameManager.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public static GameManager instance = null;
+
+    public BoardManager boardScript;
+    private int level = 3;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
 }
---
+++ AndroidGame/Assets/Scripts/Loader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+/*
+ *  Check if GameManager is instantiated
+ */
+public class Loader : MonoBehaviour {
+
+    public GameObject gameManager;
+
+	void Awake()
+    {
+        if (GameManager.instance == null)
+            Instantiate(gameManager);
+    }
+}
